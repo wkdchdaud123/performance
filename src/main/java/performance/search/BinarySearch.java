da7a6cc94fea6a659ced26b8455b7fcc108a9ab3
wkdchdaud123 <wkdchdaud12@nate.com>
2021-05-17 22:17:38 +0900

    search internal flow confirm

---
+++ src/main/java/performance/search/BinarySearch.java
@@ -0,0 +1,38 @@
+package performance.search;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] intArrays = new int[]{1,3,5,6,7,9};
+
+        int pl = 2000000000;
+        int pr = 2100000000;
+        int pc = (pl + pr) / 2;
+        System.out.println( pc ); // 2050000000 저는 이렇게 나왔으면 하는데 -97483648 이렇게 나오는데...
+
+        // 아래와 같이 쉬프트 하면 된다. ㅎㅎ
+        int pcd = pl + pr >>> 1;
+        System.out.println(pcd);
+
+
+        System.out.println(Integer.MIN_VALUE);
+
+        String indexOfalgorithmTest = "test";
+        int intIndexOfalgorithmTest = 1234;
+        System.out.println(indexOfalgorithmTest.indexOf("est"));
+
+        List list = new ArrayList();
+        list.indexOf();
+
+
+
+
+    }
+
+
+}
--- src/main/java/performance/thisTest.java
+++ src/main/java/performance/thisTest.java
@@ -1,11 +1,19 @@
 package performance;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class thisTest {
 
 
     public static void main(String[] args) {
         new thisTest(10,20,30,400);
 
+        int sum = 0;
+        for(int i = 0; i < 2; i++){
+            sum += i;
+        }
+        System.out.println(sum/2);
     }
 
 
